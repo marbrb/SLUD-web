22e9b7c0d047ad1bbcd51c8a55a393b089029019
Nestor Sanchez <nestor.sanchez94@gmail.com>
2018-11-09 16:43:08 -0500

    Hecha la clase 2

--- charlas/admin.py
+++ charlas/admin.py
@@ -3,31 +3,15 @@ from django.contrib import admin
 from .models import Charla
 from .models import Ponente
 
+# Register your models here.
+
 class PonenteAdmin(admin.ModelAdmin):
-    list_display = (
-        'nombre',
-        'trabajo'
-    )
-
-    search_fields = (
-        'nombre',
-    )
-
+    list_display=('nombre', 'trabajo')
+    search_fields=('nombre',)
 
 class CharlaAdmin(admin.ModelAdmin):
-    list_display = (
-        'titulo',
-        'descripcion',
-        'fecha',
-        'hora',
-        'lugar',
-    )
-
-    search_fields = (
-        'titulo',
-        'descripcion',
-    )
-
+    list_display=('titulo', 'descripcion', 'fecha', 'hora', 'lugar')
+    search_fields=('titulo', 'descripcion',)
 
 admin.site.register(Ponente, PonenteAdmin)
 admin.site.register(Charla, CharlaAdmin)
--- charlas/models.py
+++ charlas/models.py
@@ -1,36 +1,37 @@
 from django.db import models
 
+# Create your models here.
 
 class Ponente(models.Model):
     nombre = models.CharField(
-        max_length=100
+        max_length = 100
     )
 
     trabajo = models.CharField(
-        max_length=100,
-        blank=True
+        max_length = 100,
+        blank = True
     )
 
     foto = models.URLField(
-        max_length=400,
-        blank=True
+        max_length = 400,
+        blank = True
     )
 
     prioridad = models.IntegerField(
-        default=0
-    )
+        default = 0
+    ) 
 
     def __str__(self):
         return self.nombre
 
-
 class Charla(models.Model):
+
     titulo = models.CharField(
-        max_length=100
+        max_length = 100
     )
 
     descripcion = models.TextField(
-        max_length=255
+        max_length = 255
     )
 
     ponentes = models.ManyToManyField(
@@ -38,12 +39,12 @@ class Charla(models.Model):
     )
 
     fecha = models.DateField()
-
+    
     hora = models.TimeField()
 
     lugar = models.CharField(
-        max_length=150
+        max_length = 150
     )
 
     def __str__(self):
-        return self.titulo
+        return selt.titulo
